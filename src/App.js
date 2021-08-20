b7760c048f30da5dd0b51caf43c3b440fe07604f
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-20 14:12:38 -0400

    Completed App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Login from "./components/Login";
 import "./styles.scss";
+import Login from "./components/Login";
+import React, { useState } from "react";
+import PrivateRoute from "./components/PrivateRoute";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import BubblePage from "./components/BubblePage";
 
 function App() {
+  const logout = () => {
+    localStorage.removeItem("token");
+  };
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a
+            onClick={logout}
+            data-testid="logoutButton"
+            href="http://localhost:3000"
+          >
+            logout
+          </a>
         </header>
+        <PrivateRoute exact path="/bubblepage" component={BubblePage} />
       </div>
     </Router>
   );
