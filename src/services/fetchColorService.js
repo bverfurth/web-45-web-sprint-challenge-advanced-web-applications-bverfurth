1d0d5938669035c5c52a7dfcf7d96521639598e3
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-20 14:15:51 -0400

    Finished Fetch Color Service

--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,7 +1,16 @@
-import axiosWithAuth from '../helpers/axiosWithAuth';
+import axiosWithAuth from "../helpers/axiosWithAuth";
 
 const fetchColorService = () => {
-    
-}
+  return axiosWithAuth()
+    .get("/api/colors")
+    .then((res) => {
+      console.log("fetchColorService", res);
+
+      return res.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 export default fetchColorService;
